dcffe1d036e863ae63e490e1b21ae51da3f95821
meganhodge <mhodge16@uw.edu>
2015-10-07 12:51:25 -0700

    Initial Commit

---
+++ SimpleCalc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimpleCalc
+//
+//  Created by Megan Hodge on 10/7/15.
+//  Copyright © 2015 Megan Hodge. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
